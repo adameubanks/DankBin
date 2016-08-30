32b49bc79f6a0be240f1ad6e0033b79540037569
Adam <akhanrade@gmail.com>
2016-08-30 15:26:28 -0400

    Added More Active Admin Privilages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 AdminUser.create!(email: 'akhanrade@gmail.com', password: 'eleph@nt1118', password_confirmation: 'eleph@nt1118')
