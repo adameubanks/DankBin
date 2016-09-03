122a53761e9d0bd4d646afa11882164f3a2de44d
Adam <akhanrade@gmail.com>
2016-09-03 17:38:50 -0400

    Tuned up post rank algorithm

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
   has_many :comments
+  scope :five_days_ago, lambda { where("created_at >= :date", :date => 5.days.ago) }
+  scope :later, lambda { where("created_at <= :date", :date => 5.days.ago) }
 end
